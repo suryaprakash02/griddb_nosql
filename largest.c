c9e7646dcd17b847c401dd3111a8aee59c23c249
suryaprakash02 <47514963+suryaprakash02@users.noreply.github.com>
2019-02-20 14:11:33 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("number is a,b");
+scanf("%d%d",&a,&b,);
+if(a>b)
+{
+printf("a is greater");
+else
+printf("b is greater");
+}}
